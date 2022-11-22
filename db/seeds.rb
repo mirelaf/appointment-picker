65528b1bc8abcad661bd5f238cbc590f5b2a6c61
mirelaf <mirela.ffr@gmail.com>
2022-11-22 21:12:59 +0100

    Add db seeds with sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Location.create(day_start: DateTime.parse("8am"),
+                day_end: DateTime.parse("18pm"),
+                lunch_start: DateTime.parse("12pm"),
+                lunch_end: DateTime.parse("1pm"),
+                appointment_duration_minutes: 30)
+
+WEEKLY_APPOINTMENTS = [
+  { from: "2021-01-04T10:15:00", to: "2021-01-04T10:30:00" },
+  { from: "2021-01-05T11:00:00", to: "2021-01-05T11:30:00" },
+  { from: "2021-01-05T15:30:00", to: "2021-01-05T16:30:00" },
+  { from: "2021-01-06T10:00:00", to: "2021-01-06T10:30:00" },
+  { from: "2021-01-06T11:00:00", to: "2021-01-06T12:30:00" },
+  { from: "2021-01-06T17:30:00", to: "2021-01-06T18:00:00" },
+]
+
+WEEKLY_APPOINTMENTS.each do |period|
+  Appointment.create(from: DateTime.parse(period[:from]),
+                    to: DateTime.parse(period[:to]),
+                    location: Location.first)
+end
