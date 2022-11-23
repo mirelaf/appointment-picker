f6dc739d7100d4be17fee1f534a55fb334e9545a
mirelaf <mirela.ffr@gmail.com>
2022-11-23 23:36:16 +0100

    Separate location requests specs

--- spec/requests/locations/get_locations_spec.rb
+++ spec/requests/locations/get_locations_spec.rb
@@ -9,13 +9,4 @@ RSpec.describe 'Locations', type: :request do
       expect(response.body).to include(location.id.to_s)
     end  
   end
-
-  describe 'GET /location.json' do
-    it "includes the location and the search range" do
-      location = create(:location)
-      get location_path(location), params: { start_date: "2022-10-20", end_date: "2022-10-21" }
-
-      expect(response.body).to include("20.10.2022", "21.10.2022", location.id.to_s)
-    end  
-  end
 end
