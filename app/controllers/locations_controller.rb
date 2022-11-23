b699db4c0aa1f79812711f663459792a1e084fa7
mirelaf <mirela.ffr@gmail.com>
2022-11-23 23:32:17 +0100

    Refactor unavailable slots into separate method

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -8,8 +8,7 @@ class LocationsController < ApplicationController
 
     @start_date = params[:start_date].present? ? Date.parse(params[:start_date]) : Date.parse("2021-01-04")
     @end_date = params[:end_date].present? ? Date.parse(params[:end_date]) : Date.parse("2021-01-07")
-    @appointments_already_booked = @location.appointments
-                                            .where(from: @start_date.beginning_of_day..@end_date.end_of_day)
-    @location_available_slots = @location.available_slots(@start_date, @end_date)
+    @unavailable_slots = @location.unavailable_slots(@start_date, @end_date)
+    @available_slots = @location.available_slots(@start_date, @end_date)
   end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -4,39 +4,45 @@ class Location < ApplicationRecord
   validates_presence_of :day_start, :day_end, :lunch_start, :lunch_end, :appointment_duration_minutes
   validate :times_should_be_in_chronological_order
   
+  def unavailable_slots(start_date, end_date)
+    unavailable_slots = []
+
+    (start_date..end_date).to_a.each do |day|
+      # select the already booked appointments for the day
+      appointments.select { |appointment| appointment.from.to_date == day}.each do |appointment|
+        unavailable_slots << { from: appointment.from, to: appointment.to }
+      end
+      # include the lunch break as unavailable slot, too
+      unavailable_slots << { from: parse_time(day, lunch_start), to: parse_time(day, lunch_end) }
+    end
+
+    # sort by the "from" so that the unavailable slot are ordered chronologically  
+    unavailable_slots.sort_by! { |slot| slot[:from] }
+  end
+
   def available_slots(start_date, end_date)
     available_slots = []
   
     (start_date..end_date).to_a.each do |day|
       slot_start = parse_time(day, day_start)
       day_end_time = parse_time(day, day_end)
-      day_unavailable_periods = []
-  
-      # select the already booked appointments for the day
-      appointments.select { |appointment| appointment.from.to_date == day}.each do |appointment|
-        day_unavailable_periods << { from: appointment.from, to: appointment.to }
-      end
-
-      # include the lunch break as unavailable slot, too
-      day_unavailable_periods << { from: parse_time(day, lunch_start), to: parse_time(day, lunch_end) }
-      # sort by the "from" so that the unavailable periods are ordered chronologically
-      day_unavailable_periods.sort_by! { |period| period[:from] }
+      day_unavailable_slots = unavailable_slots(day, day)
 
       while slot_start < day_end_time do
         slot_end = slot_start + appointment_duration_minutes.minutes
 
-        # find the next unavailable period for the day
-        next_unavailable_period = day_unavailable_periods.select { |period| period[:from] >= slot_start }.first
+        # find the next unavailable slot for the day
+        next_unavailable_slot = day_unavailable_slots.select { |slot| slot[:from] >= slot_start }.first
 
-        if next_unavailable_period.nil? # nice, the rest of the day is free!
+        if next_unavailable_slot.nil? # nice, the rest of the day is free!
           available_slots << { from: slot_start, to: slot_end } if slot_end <= day_end_time 
           slot_start = slot_end
         else 
-          unless next_unavailable_period[:from] < slot_end && slot_start < next_unavailable_period[:to] # the slot overlaps an unavailable period
+          unless next_unavailable_slot[:from] < slot_end && slot_start < next_unavailable_slot[:to] # the slot overlaps an unavailable slot
             available_slots << { from: slot_start, to: slot_end } if slot_end <= day_end_time 
             slot_start = slot_end
-          else # start at the end of the unanavailable period, and if during lunch time, after lunch time
-            slot_start = next_unavailable_period[:to].between?(parse_time(day, lunch_start), parse_time(day, lunch_end)) ? parse_time(day, lunch_end) : next_unavailable_period[:to]
+          else # start at the end of the unanavailable slot, and if during lunch time, after lunch time
+            slot_start = next_unavailable_slot[:to].between?(parse_time(day, lunch_start), parse_time(day, lunch_end)) ? parse_time(day, lunch_end) : next_unavailable_slot[:to]
           end
         end
       end
