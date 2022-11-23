dcda97ad3a3442aff6120e48a4e42b49f48c4191
mirelaf <mirela.ffr@gmail.com>
2022-11-23 22:57:33 +0100

    Add request specs for locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -6,8 +6,8 @@ class LocationsController < ApplicationController
   def show
     @location = Location.find(params[:id])
 
-    @start_date = Date.parse("2021-01-04") # hard-coded until made selectable / user-editable
-    @end_date = Date.parse("2021-01-07") # hard-coded until made selectable / user-editable
+    @start_date = params[:start_date].present? ? Date.parse(params[:start_date]) : Date.parse("2021-01-04")
+    @end_date = params[:end_date].present? ? Date.parse(params[:end_date]) : Date.parse("2021-01-07")
     @appointments_already_booked = @location.appointments
                                             .where(from: @start_date.beginning_of_day..@end_date.end_of_day)
     @location_available_slots = @location.available_slots(@start_date, @end_date)
