f6973fed54896d001657074f78c16df008825a70
mirelaf <mirela.ffr@gmail.com>
2022-11-24 17:42:48 +0100

    Add location edit action and form

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,6 +3,18 @@ class LocationsController < ApplicationController
     @locations = Location.all
   end
 
+  def edit
+    @location = Location.find(params[:id])
+  end
+
+  def update
+    @location = Location.find(params[:id])
+
+    return redirect_to location_path(@location) if @location.update(location_params)
+
+    render 'edit'
+  end
+
   def show
     @location = Location.find(params[:id])
   end
@@ -15,5 +27,13 @@ class LocationsController < ApplicationController
     @available_slots = @location.available_slots(@start_date, @end_date)
     
     render 'show'
+  end
+
+  private
+
+  def location_params
+    params.require(:location)
+      .permit(:day_start, :day_end, :lunch_start,
+              :lunch_end, :appointment_duration_minutes)
   end
 end
